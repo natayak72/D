d53852ed853c244e77f2e201ddb11cea175e0f9d
Павел Харитончик (Москва) <hpo@avz-center.ru>
2023-05-19 14:28:42 +0300

    добавлено нормальное отображение моделей в админке

--- news/admin.py
+++ news/admin.py
@@ -4,9 +4,29 @@ from django.contrib import admin
 from .models import *
 
 
-# Register your models here.
-admin.site.register(Author)
-admin.site.register(Category)
-admin.site.register(Post)
-admin.site.register(PostCategory)
-admin.site.register(Comment)
+class PostAdmin(admin.ModelAdmin):
+    # list_display — это список или кортеж со всеми полями, которые вы хотите видеть в таблице с товарами
+    list_display = ('author', 'header', 'rating', 'create_datetime', 'type')
+    list_filter = ['category', 'author']
+    search_fields = ['header']
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('authorUser', 'rating')
+    list_filter = ('authorUser',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', )
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('post', 'author', 'create_datetime', 'rating', )
+    list_filter = ('author', )
+
+
+
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
